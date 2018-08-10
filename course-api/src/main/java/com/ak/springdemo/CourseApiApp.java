adb08fdaa2e4cf7fc6b13a5ce7bedb61248dc5ad
Akshay Tilekar <akshay.tilekar17@gmail.com>
2018-08-10 13:50:35 +0530

    Added demo Spring boot Application for Course API service

---
+++ course-api/src/main/java/com/ak/springdemo/CourseApiApp.java
@@ -0,0 +1,13 @@
+package com.ak.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	public static void main (String [] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+	
+}
